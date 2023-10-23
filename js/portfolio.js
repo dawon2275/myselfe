38b0656d9fe53ba0a918a46894eb0219a8010dd2
dawon2275 <dawon2275@gmail.com>
2023-10-23 10:19:26 +0900

    first react shopping-mall project

---
+++ js/portfolio.js
@@ -0,0 +1,39 @@
+$('.hom').on('mouseover',function(){
+    $('.tree').attr('src','img/palm2.png')
+})
+$('.hom').on('mouseout',function(){
+    $('.tree').attr('src','img/palm.png')
+})
+
+
+
+$(function() {
+    $('.home').animate({
+        top: 0,
+        opacity: 1
+    })
+})
+
+
+$(function() {
+    let tab = $('.tab li');
+
+    tab.on('click', function(){
+        let idx = $(this).index();
+        let tab_cont = $(this).parents('.tab').siblings('.tab_content').eq(idx);
+
+        //console.log(idx)
+        //console.log(tab_cont)
+        //1. 클릭된 li에 on 클래스 추가
+        $(this).addClass('on');
+
+        //현재 클릭된 li의 형제요소들은 클래스 제거
+        $(this).siblings().removeClass('on')
+
+        //tab_content에도 add/remove클래스 적용
+        tab_cont.addClass('on');
+        tab_cont.siblings().removeClass('on');
+    })
+    
+
+});
